e66af72c484bed892fbb2f6176b50751d6575097
satriaprayoga <satria.prayoga@gmail.com>
2020-05-17 05:39:53 +0700

    change name

--- travis_scripts/docker_push.sh
+++ travis_scripts/docker_push.sh
@@ -1,10 +1,10 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push infosys/infosys-eureka:$BUILD_NAME
-docker push infosys/infosys-gateway:$BUILD_NAME
-docker push infosys/infosys-oauth:$BUILD_NAME
-docker push infosys/infosys-message:$BUILD_NAME
-docker push infosys/infosys-customer:$BUILD_NAME
-docker push infosys/infosys-destination:$BUILD_NAME
-docker push infosys/infosys-search:$BUILD_NAME
-docker push infosys/infosys-webclient:$BUILD_NAME
+docker push satriaprayoga/infosys-eureka:$BUILD_NAME
+docker push satriaprayoga/infosys-gateway:$BUILD_NAME
+docker push satriaprayoga/infosys-oauth:$BUILD_NAME
+docker push satriaprayoga/infosys-message:$BUILD_NAME
+docker push satriaprayoga/infosys-customer:$BUILD_NAME
+docker push satriaprayoga/infosys-destination:$BUILD_NAME
+docker push satriaprayoga/infosys-search:$BUILD_NAME
+docker push satriaprayoga/infosys-webclient:$BUILD_NAME
