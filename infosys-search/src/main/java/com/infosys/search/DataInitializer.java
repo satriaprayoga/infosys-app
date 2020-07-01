8a1ae0d384ac32fd86195728712256e0336704d1
satriaprayoga <satria.prayoga@gmail.com>
2020-07-01 11:25:05 +0700

    fix initializer

--- infosys-search/src/main/java/com/infosys/search/DataInitializer.java
+++ infosys-search/src/main/java/com/infosys/search/DataInitializer.java
@@ -7,9 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
@@ -24,8 +22,8 @@ import com.infosys.search.service.LandmarkService;
 import com.infosys.search.service.TourService;
 
 
-@Component
-public class DataInitializer implements CommandLineRunner{
+//@Component
+public class DataInitializer /*implements CommandLineRunner*/{
 
 	@Value("classpath:data/tour.json")
 	private Resource tourJsonFile;
@@ -62,8 +60,8 @@ public class DataInitializer implements CommandLineRunner{
 		land.forEach(landmarkService::create);
 		List<Destination> dest=this.loadDestinationfromfile();
 		dest.forEach(destinationService::create);
-		List<Hotel> hotels=this.loadHotelfromfile();
-		hotels.forEach(hotelService::create);
+		//List<Hotel> hotels=this.loadHotelfromfile();
+		//hotels.forEach(hotelService::create);
 	
 
 
